197a55a8c8c9e158ae0847466197567ae6c39247
Joan Luna <alexis_dx8@hotmail.com>
2021-12-27 18:57:38 -0700

    Agregando Modelos y modificando

--- Controllers/EscuelaController.cs
+++ Controllers/EscuelaController.cs
@@ -8,7 +8,8 @@ namespace platzi_asp_net_core.Controllers
         public IActionResult Index()
         {
             var escuela = new Escuela();
-            
+            escuela.AñoDeCreación=2005;
+            escuela.UniqueId = Guid.NewGuid().ToString();
             escuela.Nombre ="Platzi School";
 
             ViewBag.CosaDinamica = "La Monja locoshona";
